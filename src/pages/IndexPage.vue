04e9d9541696a3b99f03a86459512e483f10545c
Husni Mubarok <niemubarok@gmail.com>
2022-03-25 16:32:11 +0700

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+   <q-btn label="Select Design"></q-btn>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
